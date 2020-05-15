87e62359a19a4165b03abd5b6a075c0515d32ecf
JohnWich-Zeus <warcraft30614@qq.com>
2020-05-15 15:47:41 +0800

    Update SSMApplication.java

--- src/main/java/com/study/ssm/SSMApplication.java
+++ src/main/java/com/study/ssm/SSMApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsmApplication {
+public class SSMApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SsmApplication.class, args);
+        SpringApplication.run(SSMApplication.class, args);
     }
 
 }
